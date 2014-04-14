d8c952d878495b92bfe2ab7d71fe16d8a5455f1a
achingbrain <alex@achingbrain.net>
2014-04-14 17:43:51 +0100

    keep id secret from extending classes

--- test/ClientTest.js
+++ test/ClientTest.js
@@ -14,7 +14,7 @@ var Client = proxyquire("../lib/Client", stubs);
 
 describe("Client", function() {
   describe("connect", function() {
-    it("should connect to the game server", function(done) {
+    it("should connect to the game server and emit the connected event", function(done) {
       var rpcPort = 10;
       var id = "an_id";
 
@@ -22,26 +22,29 @@ describe("Client", function() {
         connect: sinon.stub()
       };
 
-      remote.connect.callsArgWith(1, null, id);
+      // make dnode's connect call it's callback after a little while
+      stubs.dnode.connect.callsArgWithAsync(1, remote);
 
-      stubs.dnode.connect.callsArgWith(1, remote);
+      // after connecting, we call connect on the server to get our ID
+      remote.connect.callsArgWithAsync(1, null, id);
 
       var client = new Client({
         rpc: {
           port: rpcPort
         }
       });
+      client.on("connected", function() {
+        // should have called connect
+        stubs.dnode.connect.callCount.should.equal(1);
 
-      // should have called connect
-      stubs.dnode.connect.callCount.should.equal(1);
+        // should have connected to the right port
+        stubs.dnode.connect.getCall(0).args[0].should.equal(rpcPort);
 
-      // should have connected to the right port
-      stubs.dnode.connect.getCall(0).args[0].should.equal(rpcPort);
+        // should have stored the assigned id
+        client._id.should.equal(id);
 
-      // should have stored the assigned id
-      client._id.should.equal(id);
-
-      done();
+        done();
+      });
     });
   })
 })
